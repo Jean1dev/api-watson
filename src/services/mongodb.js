30afa0db0073d4216169d65e381a962d75e256ce
Jeanluca <jeanlucafp@gmail.com>
2020-07-10 20:01:10 -0300

    Update mongodb.js

--- src/services/mongodb.js
+++ src/services/mongodb.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const DATABASE_NAME = 'test'
-const uri = process.env.MONGO_URL || 'mongodb+srv://admin:admin@if-tecnologia-mxonm.gcp.mongodb.net/test?retryWrites=true&w=majority'
+const uri = process.env.MONGO_URL
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 class MongoUtils {
